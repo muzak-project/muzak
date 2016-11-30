47f8d0d06119a0953f1b6c6fdc64209c75082af0
William Woodruff <william@tuffbizz.com>
2016-11-30 12:21:57 -0500

    muzak: Make tab completion slightly smarter.

--- muzak.rb
+++ muzak.rb
@@ -12,11 +12,14 @@ muzak = Muzak::Instance.new(opts)
 
 COMMANDS = Muzak::Cmd.humanize_commands!
 
+ARTIST_REGEX = Regexp.union COMMANDS.select{ |c| c =~ /artist/ }.map { |c| /^#{c} / }
+ALBUM_REGEX = Regexp.union COMMANDS.select{ |c| c =~ /album/ }.map { |c| /^#{c} / }
+
 comp = proc do |s|
   case Readline.line_buffer
-  when /^enqueue-artist /
+  when ARTIST_REGEX
     muzak.index.artists.grep(Regexp.new(Regexp.escape(s)))
-  when /^enqueue-album /
+  when ALBUM_REGEX
     muzak.index.album_names.grep(Regexp.new(Regexp.escape(s)))
   else
     COMMANDS.grep(Regexp.new(Regexp.escape(s)))

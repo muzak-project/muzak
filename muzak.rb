01e354de59b7065c9ba8c89f9948d573c06db315
William Woodruff <william@tuffbizz.com>
2016-12-04 15:48:11 -0500

    muzak: Add tab completion for playlist commands.

--- muzak.rb
+++ muzak.rb
@@ -17,6 +17,7 @@ COMMANDS = Muzak::Cmd.humanize_commands!
 CONFIG_REGEX = /^config-(get)|(set)|(del)/
 ARTIST_REGEX = Regexp.union COMMANDS.select{ |c| c =~ /artist/ }.map { |c| /^#{c}/ }
 ALBUM_REGEX = Regexp.union COMMANDS.select{ |c| c =~ /album/ }.map { |c| /^#{c}/ }
+PLAYLIST_REGEX = /^playlist-(load)|(delete)/
 
 comp = proc do |s|
   case Readline.line_buffer
@@ -27,6 +28,8 @@ comp = proc do |s|
     muzak.index.artists.grep(Regexp.new(Regexp.escape(ss[1..-1].join(" "))))
   when ALBUM_REGEX
     muzak.index.album_names.grep(Regexp.new(Regexp.escape(s)))
+  when PLAYLIST_REGEX
+    Muzak::Playlist.playlist_names.grep(Regexp.new(Regexp.escape(s)))
   else
     COMMANDS.grep(Regexp.new(Regexp.escape(s)))
   end

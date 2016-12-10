cecd4d4888b5021b186d99763b81349905f87a76
William Woodruff <william@tuffbizz.com>
2016-12-09 20:50:53 -0500

    const: Bump muzak to 0.0.6.

--- lib/muzak/const.rb
+++ lib/muzak/const.rb
@@ -1,5 +1,5 @@
 module Muzak
-  VERSION = "0.0.5".freeze
+  VERSION = "0.0.6".freeze
 
   CONFIG_DIR = File.expand_path("~/.config/muzak").freeze
   CONFIG_FILE = File.join(CONFIG_DIR, "muzak.yml").freeze

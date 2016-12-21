976cdcd81a79ae77a00d55d9b4790c07478074b1
William Woodruff <william@tuffbizz.com>
2016-12-21 00:27:50 -0500

    player/stub_player: Document Muzak::Player::StubPlayer.

--- lib/muzak/player/stub_player.rb
+++ lib/muzak/player/stub_player.rb
@@ -1,70 +1,121 @@
 module Muzak
   module Player
+    # A no-op player that all players inherit from.
     class StubPlayer
       include Utils
 
+      # @return [Instance] the instance associated with this player
       attr_reader :instance
 
+      # @param instance [Instance] the instance associated with the player
       def initialize(instance)
         @instance = instance
       end
 
+      # @return [false] whether or not the player is running
+      # @note NO-OP
       def running?
         debug "#running?"
+        false
       end
 
+      # Activates the player.
+      # @return [void]
+      # @note NO-OP
       def activate!
         debug "#activate!"
       end
 
+      # Deactivates the player.
+      # @return [void]
+      # @note NO-OP
       def deactivate!
         debug "#deactivate!"
       end
 
+      # Starts playback.
+      # @return [void]
+      # @note NO-OP
       def play
         debug "#play"
       end
 
+      # Ends playback.
+      # @return [void]
+      # @note NO-OP
       def pause
         debug "#pause"
       end
 
+      # @return [false] whether or not the player is currently playing
+      # @note NO-OP
       def playing?
         debug "#playing?"
+        false
       end
 
+      # Moves to the next song.
+      # @return [void]
+      # @note NO-OP
       def next_song
         debug "#next_song"
       end
 
+      # Moves to the previous song.
+      # @return [void]
+      # @note NO-OP
       def previous_song
         debug "#previous_song"
       end
 
+      # Enqueues the given song.
+      # @param song [Song] the song to enqueue
+      # @return [void]
+      # @note NO-OP
       def enqueue_song(song)
         debug "#enqueue_song"
       end
 
+      # Enqueues the given album.
+      # @param album [Album] the album to enqueue
+      # @return [void]
+      # @note NO-OP
       def enqueue_album(album)
         debug "#enqueue_album"
       end
 
+      # Enqueues the given playlist.
+      # @param playlist [Playlist] the playlist to enqueue
+      # @return [void]
+      # @note NO-OP
       def enqueue_playlist(playlist)
         debug "#enqueue_playlist"
       end
 
+      # List the player's queue.
+      # @return [void]
+      # @note NO-OP
       def list_queue
         debug "#list_queue"
       end
 
+      # Shuffle the player's queue.
+      # @return [void]
+      # @note NO-OP
       def shuffle_queue
         debug "#shuffle_queue"
       end
 
+      # Clear the player's queue.
+      # @return [void]
+      # @note NO-OP
       def clear_queue
         debug "#clear_queue"
       end
 
+      # Get the currently playing song.
+      # @return [void]
+      # @note NO-OP
       def now_playing
         debug "#now_playing"
       end

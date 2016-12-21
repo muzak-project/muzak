5fb2256a08942e6a012309659e38036689e70189
William Woodruff <william@tuffbizz.com>
2016-12-20 20:41:51 -0500

    cmd: Document index and playlist commands.
    Also, remove old uses of _playlists_loaded?

--- lib/muzak/cmd/index.rb
+++ lib/muzak/cmd/index.rb
@@ -1,5 +1,8 @@
 module Muzak
   module Cmd
+    # Rebuild the current instance's index.
+    # @command `index-build`
+    # @cmdexample `muzak> index-build`
     def index_build(*args)
       warn_arity(args, 0)
 
@@ -8,18 +11,27 @@ module Muzak
       index.build!
     end
 
+    # List all artists in the index.
+    # @command `list-artists`
+    # @cmdexample `muzak> list-artists`
     def list_artists(*args)
       warn_arity(args, 0)
 
       puts index.artists.join("\n")
     end
 
+    # List all albums in the index.
+    # @command `list-albums`
+    # @cmdexample `muzak> list-albums`
     def list_albums(*args)
       warn_arity(args, 0)
 
       puts index.album_names.join("\n")
     end
 
+    # List all albums by the given artist in the index.
+    # @command `albums-by-artist <artist name>`
+    # @cmdexample `muzak> albums-by-artist Your Favorite Artist`
     def albums_by_artist(*args)
       artist = args.join(" ")
       return if artist.nil?
@@ -27,6 +39,9 @@ module Muzak
       puts index.albums_by(artist).map(&:title)
     end
 
+    # List all songs by the given artist in the index.
+    # @command `songs-by-artist <artist name>`
+    # @cmdexample `muzak> songs-by-artist Your Next Favorite Artist`
     def songs_by_artist(*args)
       artist = args.join(" ")
       return if artist.nil?
--- lib/muzak/cmd/playlist.rb
+++ lib/muzak/cmd/playlist.rb
@@ -1,11 +1,17 @@
 module Muzak
   module Cmd
+    # List all currently available playlists.
+    # @command `list-playlists`
+    # @cmdexample `muzak> list-playlists`
     def list_playlists(*args)
       Playlist.playlist_names.each do |playlist|
         info playlist
       end
     end
 
+    # Delete the given playlist.
+    # @command `playlist-delete <playlist>`
+    # @cmdexample `muzak> playlist-delete favorites`
     def playlist_delete(*args)
       fail_arity(args, 1)
       pname = args.shift
@@ -16,6 +22,9 @@ module Muzak
       playlists[pname] = nil
     end
 
+    # Add the given playlist to the player's queue.
+    # @command `enqueue-playlist <playlist>`
+    # @cmdexample `muzak> enqueue-playlist favorites`
     def enqueue_playlist(*args)
       fail_arity(args, 1)
       pname = args.shift
@@ -24,6 +33,9 @@ module Muzak
       event :playlist_enqueued, playlists[pname]
     end
 
+    # Add the given album to the given playlist.
+    # @command `playlist-add-album <playlist> <album name>`
+    # @cmdexample `muzak> playlist-add-album favorites Your Favorite Album`
     def playlist_add_album(*args)
       pname = args.shift
       return if pname.nil?
@@ -37,6 +49,9 @@ module Muzak
       playlists[pname].add(album.songs)
     end
 
+    # Add the given artist to the given playlist.
+    # @command `playlist-add-artist <playlist> <artist name>`
+    # @cmdexample `muzak> playlist-add-artist dad-rock The Rolling Stones`
     def playlist_add_artist(*args)
       pname = args.shift
       return if pname.nil?
@@ -47,8 +62,12 @@ module Muzak
       playlists[pname].add(index.songs_by(artist))
     end
 
+    # Add the currently playing song to the given playlist.
+    # @see Muzak::Player::StubPlayer#now_playing
+    # @command `playlist-add-current <playlist>`
+    # @cmdexample `muzak> playlist-add-current favorites`
     def playlist_add_current(*args)
-      return unless player.running? && _playlists_loaded?
+      return unless player.running?
 
       pname = args.shift
       return if pname.nil?
@@ -56,8 +75,12 @@ module Muzak
       playlists[pname].add player.now_playing
     end
 
+    # Deletes the currently playing song from the given playlist.
+    # @see Muzak::Player::StubPlayer#now_playing
+    # @command `playlist-del-current <playlist>`
+    # @cmdexample `muzak> playlist-del-current favorites`
     def playlist_del_current(*args)
-      return unless player.running? && _playlists_loaded?
+      return unless player.running?
 
       pname = args.shift
       return if pname.nil?
@@ -65,6 +88,10 @@ module Muzak
       playlists[pname].delete player.now_playing
     end
 
+    # Shuffle the given playlist.
+    # @see Muzak::Playlist#shuffle!
+    # @command `playlist-shuffle <playlist>`
+    # @cmdexample `muzak> playlist-shuffle dad-rock`
     def playlist_shuffle(*args)
       pname = args.shift
       return if pname.nil?

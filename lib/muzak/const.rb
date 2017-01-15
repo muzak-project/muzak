51b0120c88ef66d8a2da280c263f4ac473484923
William Woodruff <william@tuffbizz.com>
2017-01-14 19:22:33 -0500

    const: Bump muzak to 0.2.4.

--- lib/muzak/const.rb
+++ lib/muzak/const.rb
@@ -1,6 +1,6 @@
 module Muzak
   # Muzak's current version
-  VERSION = "0.2.3".freeze
+  VERSION = "0.2.4".freeze
 
   # The root directory for all user configuration, data, etc
   CONFIG_DIR = File.expand_path("~/.config/muzak").freeze
